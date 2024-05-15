27d48cd8e19a7c2c9dbd3c3a25837af2ec8b78d7
Chris Scarcelle <chris.scarcelle@bentley.com>
2024-05-15 09:25:26 -0400

    Initial git sync

--- main.tf
+++ main.tf
@@ -1,113 +1,157 @@
-terraform {
-
-  required_version = ">=0.12"
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  skip_provider_registration = "true"
-  features {}
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = "${random_pet.prefix.id}-rg"
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "TFNet" {
-    name                = "TFVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "East US"
-    resource_group_name = "<RESOURCE_GROUP_NAME>"
-
-    tags = {
-        environment = "Terraform VNET"
-    }
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${random_pet.prefix.id}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
+
 # Create subnet
-resource "azurerm_subnet" "tfsubnet" {
-    name                 = "default"
-    resource_group_name = "<RESOURCE_GROUP_NAME>"
-    virtual_network_name = azurerm_virtual_network.TFNet.name
-    address_prefixes       = ["10.0.1.0/24"]
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "${random_pet.prefix.id}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-#Deploy Public IP
-resource "azurerm_public_ip" "example" {
-  name                = "pubip1"
-  location            = "East US"
-  resource_group_name = "<RESOURCE_GROUP_NAME>"
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "${random_pet.prefix.id}-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
-  sku                 = "Basic"
 }
 
-#Create NIC
-resource "azurerm_network_interface" "example" {
-  name                = "robot-nic"
-  location            = "East US"
-  resource_group_name = "<RESOURCE_GROUP_NAME>"
+# Create Network Security Group and rules
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "${random_pet.prefix.id}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-    ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.tfsubnet.id
-    private_ip_address_allocation  = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "web"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-#Create Boot Diagnostic Account
-resource "azurerm_storage_account" "sa" {
-  name                     = "robodiags4tflab"
-  resource_group_name      = "<RESOURCE_GROUP_NAME>"
-  location                 = "East US"
-   account_tier            = "Standard"
-   account_replication_type = "LRS"
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "${random_pet.prefix.id}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-   tags = {
-    environment = "Boot Diagnostic Storage"
-    CreatedBy = "Admin"
-   }
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
+# Create virtual machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  admin_username        = "azureuser"
+  admin_password        = random_password.password.result
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
   }
 
-#Create Virtual Machine
-resource "azurerm_virtual_machine" "example" {
-  name                  = "robot"
-  location              = "East US"
-  resource_group_name   = "<RESOURCE_GROUP_NAME>"
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_B1s"
-  delete_os_disk_on_termination = true
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
     version   = "latest"
   }
 
-  storage_os_disk {
-    name              = "osdisk1"
-    disk_size_gb      = "128"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
 
-  os_profile {
-    computer_name  = "robot"
-    admin_username = "vmadmin"
-    admin_password = "Password12345!"
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
   }
+}
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+# Install IIS web server to the virtual machine
+resource "azurerm_virtual_machine_extension" "web_server_install" {
+  name                       = "${random_pet.prefix.id}-wsi"
+  virtual_machine_id         = azurerm_windows_virtual_machine.main.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
 
-boot_diagnostics {
-        enabled     = "true"
-        storage_uri = azurerm_storage_account.sa.primary_blob_endpoint
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
     }
+  SETTINGS
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+resource "random_password" "password" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
+}
+
+resource "random_pet" "prefix" {
+  prefix = var.prefix
+  length = 1
 }
